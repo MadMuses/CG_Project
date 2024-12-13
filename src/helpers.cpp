d2bdfc81b953d22917ed3c07d547746e9674454f
MadMuses <62542620+MadMuses@users.noreply.github.com>
2024-12-13 23:04:42 +0000

    Moved functions directly to main to prevent bugs

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,39 +1,6 @@
 #include "helpers.h"
 #include <tinygltf-2.9.3/stb_image.h>
 
-GLFWwindow* initOpenGL(int windowWidth, int windowHeight){
-    // Initialise GLFW
-    if (!glfwInit())
-    {
-        std::cerr << "Failed to initialize GLFW." << std::endl;
-        return NULL;
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // For MacOS
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    // Open a window and create its OpenGL context
-    GLFWwindow *window = glfwCreateWindow(windowWidth, windowHeight, "Final project", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cerr << "Failed to open a GLFW window." << std::endl;
-        glfwTerminate();
-        return NULL;
-    }
-    glfwMakeContextCurrent(window);
-
-    // Load OpenGL functions, gladLoadGL returns the loaded version, 0 on error.
-    int version = gladLoadGL(glfwGetProcAddress);
-    if (version == 0)
-    {
-        std::cerr << "Failed to initialize OpenGL context." << std::endl;
-        return NULL;
-    }
-    return window;
-};
-
 void printVec(glm::vec3 v)
 {
     std::cout << std::endl << "[" << v.x << ", " << v.y << ", " << v.z << "]" << std::endl;
@@ -83,32 +50,4 @@ std::map<std::string,GLuint> LoadShaders()
     shaderlist["objShadow"] = shadowProgramID;
 
     return shaderlist;
-}
-
-void LoadFBO(GLuint frameBufferID, GLuint depthTexture,int depthMapWidth, int depthMapHeight)
-{
-    // Create a frame buffer
-    glGenFramebuffers(1, &frameBufferID);
-    glBindFramebuffer(GL_FRAMEBUFFER, frameBufferID);
-
-    // Create Framebuffer Texture
-    glGenTextures(1, &depthTexture);
-    glBindTexture(GL_TEXTURE_2D, depthTexture);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, depthMapWidth, depthMapHeight, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // Prevents edge bleeding
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Prevents edge bleeding
-
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthTexture, 0);
-
-    glDrawBuffer(GL_NONE);
-    glReadBuffer(GL_NONE);
-
-    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-    {
-        std::cerr << "Framebuffer is not complete." << std::endl;
-    }
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
